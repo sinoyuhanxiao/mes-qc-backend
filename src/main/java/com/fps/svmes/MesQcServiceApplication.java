d4b87b60405544c05547ee01b6d1ae1b46e8243a
Erik Yu <erik.yu@fpscorp.ca>
2024-12-10 16:36:33 -0800

    QC Backend Setup

---
+++ src/main/java/com/fps/svmes/MesQcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fps.svmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesQcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesQcServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fps/svmes/MesQcServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.fps.svmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesQcServiceApplicationTests {
+
+}

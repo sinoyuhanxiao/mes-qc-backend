896bb9cfd570d0a2357701dc46bf4c81836616af
Erik Yu <erik.yu@fpscorp.ca>
2025-06-30 15:52:52 -0700

    pagination & export excel

--- src/main/java/com/fps/svmes/services/ReportingService.java
+++ src/main/java/com/fps/svmes/services/ReportingService.java
@@ -1,5 +1,6 @@
 package com.fps.svmes.services;
 
+import com.fps.svmes.dto.PagedResultDTO;
 import com.fps.svmes.dto.dtos.reporting.WidgetDataDTO;
 import org.bson.Document;
 
@@ -12,10 +13,27 @@ public interface ReportingService {
     // Now receives jsonInput instead of a list of WidgetDataDTO
     List<WidgetDataDTO> extractWidgetDataWithCounts(Long formTemplateId, String startDateTime, String endDateTime);
 
+    // Old fetchQcRecords method without pagination
     List<Document> fetchQcRecords(Long formTemplateId, String startDateTime, String endDateTime, Integer page, Integer size);
 
     List<Document> fetchQcRecordsFilteredByCreator(Long formTemplateId, String startDateTime, String endDateTime, Integer page, Integer size, Integer createdBy);
 
     List<Document> fetchAllVersionsByGroupId(Long formTemplateId, String versionGroupId);
+
+    PagedResultDTO<Document> fetchQcRecordsPaged(
+            Long formTemplateId,
+            String startDateTime,
+            String endDateTime,
+            Integer page,
+            Integer size,
+            String sort,
+            String search
+    );
+
+    List<Document> fetchAllRecordsWithoutPagination(Long formTemplateId,
+                                                           String startDateTime,
+                                                           String endDateTime,
+                                                           String search,
+                                                           String sort);
 }
 
